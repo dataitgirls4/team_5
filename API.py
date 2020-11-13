2d466ba59b0a13249772b83f9cc1af591df6746b
Heeji Kim <70740049+heezeo@users.noreply.github.com>
2020-11-13 17:41:20 +0900

    Update API.py

--- API.py
+++ API.py
@@ -9,7 +9,7 @@ import requests
 url = "https://api.pubg.com/shards/kakao/players?filter[playerNames]=de_javu_" 
 
 header = {
-  "Authorization": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIyZTUxN2M3MC0wNzhkLTAxMzktNjA3My0xM2VlZDFhM2VmZGQiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNjA1MjM3NjYyLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6ImRhdGFpdGdpcmxzIn0.TKzOoZ4svDG-sGFbMGv-uCV51jFsPAvSO0oU3nvgve4",
+  "Authorization": "Personal API",
   "Accept": "application/vnd.api+json"
 }
 
@@ -27,7 +27,7 @@ r.text
 
 url =  "https://api.pubg.com/shards/kakao/matches/24a26a10-ae7f-4cba-a717-f2b00465393e" 
 header = {
-  "Authorization": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIyZTUxN2M3MC0wNzhkLTAxMzktNjA3My0xM2VlZDFhM2VmZGQiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNjA1MjM3NjYyLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6ImRhdGFpdGdpcmxzIn0.TKzOoZ4svDG-sGFbMGv-uCV51jFsPAvSO0oU3nvgve4",
+  "Authorization": "Personal API",
   "Accept": "application/vnd.api+json"
 }
 
